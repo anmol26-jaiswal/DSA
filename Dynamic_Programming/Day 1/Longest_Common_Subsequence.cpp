1f3daf23ac661417ad8784c215aa1ea4073aea59
anmol26-jaiswal <anmol.lge@gmail.com>
2022-12-21 21:26:36 +0530

    Day 1

---
+++ Dynamic_Programming/Day 1/Longest_Common_Subsequence.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+const int mod=1e9+7;
+using namespace std;
+
+class Solution
+{
+    private:
+    int longestCommonSubsequence(int x, int y, string s1, string s2, vector<vector<int>> &dp)
+    {
+        if(x<0 || y<0)
+            return 0;
+        if(dp[x][y]!=-1)
+            return dp[x][y];
+            
+        if(s1[x]==s2[y])
+            return dp[x][y] = 1+longestCommonSubsequence(x-1, y-1, s1, s2, dp);
+            
+    
+        return dp[x][y] = max(longestCommonSubsequence(x, y-1, s1, s2, dp), longestCommonSubsequence(x-1, y, s1, s2, dp));
+    }
+    
+    public:
+    //Function to find the length of longest common subsequence in two strings.
+    int lcs(int x, int y, string s1, string s2)
+    {
+        vector<vector<int>> dp(x, vector<int>(y, -1));
+        return longestCommonSubsequence(x-1, y-1, s1, s2, dp);
+    }
+};
+
+
+int main()
+{
+    int t,n,k,x,y;
+    cin>>t;
+    while(t--)
+    {
+        cin>>x>>y;          // Take size of both the strings as input
+        string s1,s2;
+        cin>>s1>>s2;        // Take both the string as input
+        Solution ob;
+        cout << ob.lcs(x, y, s1, s2) << endl;
+    }
+    return 0;
+}
